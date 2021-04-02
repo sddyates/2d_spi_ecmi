93e1840e40d9d8c165b358daa3c1a128d15d5135
Simon Daley-Yates <sddyates@gmail.com>
2021-04-02 12:35:54 +0100

    Initial commit.

---
+++ definitions.h
@@ -0,0 +1,31 @@
+#define  PHYSICS                        MHD
+#define  DIMENSIONS                     2
+#define  GEOMETRY                       CARTESIAN
+#define  BODY_FORCE                     VECTOR
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 LINEAR
+#define  TIME_STEPPING                  HANCOCK
+#define  NTRACER                        0
+#define  PARTICLES                      NO
+#define  USER_DEF_PARAMETERS            0
+
+/* -- physics dependent declarations -- */
+
+#define  EOS                            IDEAL
+#define  ENTROPY_SWITCH                 NO
+#define  DIVB_CONTROL                   DIV_CLEANING
+#define  BACKGROUND_FIELD               YES
+#define  AMBIPOLAR_DIFFUSION            NO
+#define  RESISTIVITY                    NO
+#define  HALL_MHD                       NO
+#define  THERMAL_CONDUCTION             NO
+#define  VISCOSITY                      NO
+#define  ROTATING_FRAME                 NO
+
+/* -- user-defined parameters (labels) -- */
+
+
+/* [Beg] user-defined constants (do not change this line) */
+
+
+/* [End] user-defined constants (do not change this line) */
